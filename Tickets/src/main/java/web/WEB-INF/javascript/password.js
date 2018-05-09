8c036eee55ae45efecd81d2f5be44ae7ac4ade65
H2P <hzp_h2p@qq.com>
2018-05-09 19:12:07 +0800

    该界面
    登陆 注册界面改为 Bootstrap
    用户信息/优惠券界面 改为 Bootstrap
    
    兑换优惠券功能丧失。。。

--- Tickets/src/main/java/web/WEB-INF/javascript/password.js
+++ Tickets/src/main/java/web/WEB-INF/javascript/password.js
@@ -7,15 +7,13 @@ $("#userPassword").bind("input propertychange change", function(){
     var passwordAgain = $("#userPasswordAgain").val().toString();
     if( passwordAgain.length!==0 ){
         if( passwordAgain!==password ){
-            $("#userPassword").css("border","1px solid red");
-            $("#userPasswordAgain").css("border","1px solid red");
+            $("#userPassword").addClass("border-red");
+            $("#userPasswordAgain").addClass("border-red");
             passwordReady = false;
         }
         else {
-            $("#userPassword").css("border","1px solid #c8c8c8");
-            $("#userPassword").css("border-bottom","none");
-            $("#userPasswordAgain").css("border","1px solid #c8c8c8");
-            $("#userPasswordAgain").css("border-bottom","none");
+            $("#userPassword").removeClass("border-red");
+            $("#userPasswordAgain").removeClass("border-red");
             passwordReady = true;
         }
     }
@@ -31,15 +29,13 @@ $("#userPasswordAgain").bind("input propertychange change", function(){
 
     var password = $("#userPassword").val().toString();
     if( passwordAgain!==password ){
-        $("#userPassword").css("border","1px solid red");
-        $("#userPasswordAgain").css("border","1px solid red");
+        $("#userPassword").addClass("border-red");
+        $("#userPasswordAgain").addClass("border-red");
         passwordReady = false;
     }
     else {
-        $("#userPassword").css("border","1px solid #c8c8c8");
-        $("#userPassword").css("border-bottom","none");
-        $("#userPasswordAgain").css("border","1px solid #c8c8c8");
-        $("#userPasswordAgain").css("border-bottom","none");
+        $("#userPassword").removeClass("border-red");
+        $("#userPasswordAgain").removeClass("border-red");
         passwordReady = true;
     }
 });
